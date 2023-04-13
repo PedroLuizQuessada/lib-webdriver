7339562b00657f5ab1c4c32972e19d007dd39839
PedroLuizQuessada <pedroquessada@grupojuritas.com.br>
2023-04-13 00:15:50 -0300

    Convertendo variáveis de teste em estáticas

--- src/test/java/WebDriverTeste.java
+++ src/test/java/WebDriverTeste.java
@@ -9,10 +9,10 @@ import util.WebdriverUtil;
 public class WebDriverTeste {
 
     private final NavegadoresEnum NAVEGADOR = NavegadoresEnum.CHROME;
-    private final String WEBDRIVER_PATH = "C:\\Users\\pedroquessada\\IdeaProjects\\.pedro\\rpa-whatsapp\\RPAWhatsapp\\chromedriver.exe";
-    private final String BROWSER_EXEC_PATH = "C:\\Users\\pedroquessada\\Downloads\\Win_x64_1084030_chrome-win\\chrome-win\\";
-    private final Integer PORTA = 9222;
-    private final String PROFILE_PATH = "C:\\Users\\pedroquessada\\IdeaProjects\\.pedro\\rpa-whatsapp\\RPAWhatsapp\\profile\\";
+    private static final String WEBDRIVER_PATH = "C:\\Users\\pedroquessada\\IdeaProjects\\.pedro\\rpa-whatsapp\\RPAWhatsapp\\chromedriver.exe";
+    private static final String BROWSER_EXEC_PATH = "C:\\Users\\pedroquessada\\Downloads\\Win_x64_1084030_chrome-win\\chrome-win\\";
+    private static final Integer PORTA = 9222;
+    private static final String PROFILE_PATH = "C:\\Users\\pedroquessada\\IdeaProjects\\.pedro\\rpa-whatsapp\\RPAWhatsapp\\profile\\";
 
     @Test
     public void testeRecuperarPendencias() throws NavegadorNaoIdentificadoException, DriverException {
